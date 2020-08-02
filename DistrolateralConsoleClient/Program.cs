7621cd86d5fa725618fce3c9e0986fdd995c9ad6
Mark Rhoades-Brown <mark@rhoades-brown.co.uk>
2020-08-02 07:20:26 +0100

    gitflow-feature-stash: Initial

---
+++ DistrolateralConsoleClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using DistrolateralServer;
+using Grpc.Net.Client;
+using System;
+
+namespace DistrolateralConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:32772");
+            var client = new Greeter.GreeterClient(channel);
+            var request = new HelloRequest { Name = "Mark" };
+            Console.Write(client.SayHello(request).Message);
+
+            Console.ReadKey();
+        }
+    }
+}
