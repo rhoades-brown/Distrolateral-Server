94c95c7ab6b2f9d0ece69b886763206ad1e727be
Mark Rhoades-Brown <mark@rhoades-brown.co.uk>
2020-08-02 07:20:09 +0100

    Starting
    - Created service
    -  Created test console app
    - Added proto files
    - Added docker config
    - Started DistroService

---
+++ DistrolateralServer/Services/DistroService.cs
@@ -0,0 +1,29 @@
+﻿using DistrolateralServer.Protos;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DistrolateralServer.Services
+{
+    public class DistroService : Distrobuter.DistrobuterBase
+    {
+        private readonly ILogger<DistroService> _logger;
+        public DistroService(ILogger<DistroService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<StatusReply> StageFile(FileRequest request, ServerCallContext context)
+        {
+            return base.StageFile(request, context);
+        }
+
+        public override Task<StatusReply> TestFile(FileRequest request, ServerCallContext context)
+        {
+            return base.TestFile(request, context);
+        }
+    }
+}
