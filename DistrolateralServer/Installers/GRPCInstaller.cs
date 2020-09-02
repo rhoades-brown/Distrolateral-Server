ba14e5ef3e9e190962a9eef2a0860cd609c58377
Mark Rhoades-Brown <mark@rhoades-brown.co.uk>
2020-09-02 10:07:14 +0100

    Refactored with implementation of  'IInstallers'
    - Created IInstaller to take IServiceCollection, IConfiguration
    - Added GRPCInstaller
    - Added MvcInstalle

---
+++ DistrolateralServer/Installers/GRPCInstaller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DistrolateralServer.Installers
+{
+    public class GRPCInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddGrpc();
+        }
+    }
+}
