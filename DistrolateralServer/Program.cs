af42f9d73a2a6f07373e110fa780cb07567e1194
Mark Rhoades-Brown <mark@rhoades-brown.co.uk>
2020-09-02 10:08:28 +0100

    Improved functionality
    - Added swashbuckle for OpenAPI documentation
    - Added SeriLog logging

--- DistrolateralServer/Program.cs
+++ DistrolateralServer/Program.cs
@@ -1,22 +1,55 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
 
 namespace DistrolateralServer
 {
     public class Program
     {
+        const string DOCKER_SECRET_PATH = "/run/secrets/";
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger.Information("Application Starting...");
+
+            CreateHostBuilder(args)
+                .UseSerilog()
+                .Build()
+                .Run();
+
         }
 
         // Additional configuration is required to successfully run gRPC on macOS.
         // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "production"}.json", optional: true)
+                .AddKeyPerFile(Environment.GetEnvironmentVariable("DOCKER_SECRET_PATH") ?? DOCKER_SECRET_PATH, optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+        }
     }
 }
--- DistrolateralServer/Startup.cs
+++ DistrolateralServer/Startup.cs
@@ -1,57 +1,70 @@
-﻿using DistrolateralServer.Services;
+﻿using DistrolateralServer.Installers;
+using DistrolateralServer.Options;
+using DistrolateralServer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Linq;
 
 namespace DistrolateralServer
 {
     public class Startup
     {
-        internal IServiceCollection Build(IServiceCollection services, IConfigurationSection store)
-        {
-            if (store.Exists())
-            {
-                return (store["storeType"]) switch
-                {
-                    "s3Bucket" => services.AddSingleton<IBucketStore, S3BucketStore>(),
-                    _ => services.AddSingleton<IBucketStore, NoBucket>()
-                };
-            }
-            return services.AddSingleton<IBucketStore, NoBucket>();
-        }
-
         private readonly IConfiguration _config;
 
         public Startup(IConfiguration config)
         {
             _config = config;
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddGrpc();
-            //    services.AddSingleton<IBucketStore, NoBucket>();
-
-            Build(services, _config.GetSection("bucketStore"));
-            //services.add
-        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                //  app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            var swaggerOptions = new SwaggerOptions();
+            _config.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+            app.UseSwagger(option =>
+            {
+                option.RouteTemplate = swaggerOptions.JsonRoute;
+            });
+
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint(swaggerOptions.UiEndpoint, swaggerOptions.Description);
+            });
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            //      app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                   name: "default",
+                   pattern: "{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapGrpcService<GreeterService>();
                 endpoints.MapGrpcService<DistroService>();
                 endpoints.MapGet("/", async context =>
@@ -59,6 +72,27 @@ namespace DistrolateralServer
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
                 });
             });
+
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            typeof(Startup).Assembly.ExportedTypes
+                .Where(x =>
+                    typeof(IInstaller).IsAssignableFrom(x)
+                    && !x.IsInterface
+                    && !x.IsAbstract)
+                .Select(Activator.CreateInstance)
+                .Cast<IInstaller>()
+                .ToList()
+                .ForEach(installer =>
+                {
+                    Log.Logger.Information($"Installing: {installer.GetType().Name}");
+                    installer.InstallServices(services, _config);
+                });
         }
     }
 }
